eb01bc61dd76fa98e64f5569a787a6c10186d074
Angelo <gangelos630@gmail.com>
2025-12-07 02:00:49 -0500

    feat: develop mss start

---
+++ src/main/java/com/angelogomez/dash_bs_billing_v1/DashBsBillingV1Application.java
@@ -0,0 +1,13 @@
+package com.angelogomez.dash_bs_billing_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashBsBillingV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashBsBillingV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelogomez/dash_bs_billing_v1/DashBsBillingV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelogomez.dash_bs_billing_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashBsBillingV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
